19cd4479f12f9dcb836a20bd297480570237fc7d
sunnyp <sunny@quixom.com>
2020-02-17 11:00:44 +1100

    Adding creation of user and superuser to custom User model and test cases

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successfull"""
+        email = 'test@londonappdev.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email
+        )
+        user.set_password(password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Testing whether email is normalized or not"""
+        email = "test@LONDONAPPDEV.COM"
+        user = get_user_model().objects.create_user(
+            email,
+            'test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_user_with_invalid_email(self):
+        """Testing creation of user with invalid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'sunny@quixom.com',
+            'sunny123'
+        )
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
