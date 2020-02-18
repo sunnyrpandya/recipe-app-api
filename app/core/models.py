801530b7d171836d53f56114e5c167174d10e09f
sunnyp <sunny@quixom.com>
2020-02-18 12:14:54 +1100

    Added endpoints for creating and authenticating users

--- app/core/models.py
+++ app/core/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, passowrd=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError("User email is not provided")
         user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(passowrd)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
